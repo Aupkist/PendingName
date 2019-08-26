72e457048fd202e67fab728f35d1f7e32b48ef24
J Lamb <Dummyvariable2019@gmail.com>
2019-08-26 20:12:10 +0800

    added j

--- stdtest.cpp
+++ stdtest.cpp
@@ -1,8 +1,14 @@
 #include <map>
 #include <unordered_map>
 #include <iostream>
+#include <opencv2/core.hpp>
+
 
 int main(){
+    int i = 3;
+    int j = i+3;
+    cv::Mat Image;
+    std::cout << j << std::endl;
     std::unordered_map<int, int> Hey;
     return 0;
 }
